f8aa02baef29de1bd19b774c8c8933c9458cbd11
AndreyChekunov <79117325352@ya.ru>
2022-07-10 19:57:38 +0300

    Resolve 64

---
+++ Task_64_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+NaturalNumber(number, count);
+
+
+void NaturalNumber(int number, int count)
+{
+    if (count > number) return;
+    NaturalNumber(number, count + 1);
+    Console.Write(count + ", ");
+}
+Console.Write(count - 1);
